dc0d658e0c736bdf79c3f4764505155a16bfabe9
Aarni Koskela <aarni.koskela@andersinno.fi>
2014-04-15 20:42:25 +0300

    Actually fix setup.py.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 setup(
     name='hayes',
-    version='0.0.3',
-    packages=['hayes', 'hayes.analysis', 'hayes.ext', 'hayes.django_interop'],
+    version='0.0.3.1',
+    packages=['hayes', 'hayes.analysis', 'hayes.ext', 'hayes.django_interop', 'hayes.search'],
     license='MIT',
     long_description="n/a",
     maintainer="Aarni Koskela",
