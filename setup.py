144d88913f7f6f97487ad9c2f308a9337d99f1d9
Aarni Koskela <aarni.koskela@andersinno.fi>
2015-05-15 12:47:44 +0300

    v0.0.3.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='hayes',
-    version='0.0.3.2',
+    version='0.0.3.3',
     packages=['hayes', 'hayes.analysis', 'hayes.ext', 'hayes.django_interop', 'hayes.search'],
     license='MIT',
     long_description="n/a",
