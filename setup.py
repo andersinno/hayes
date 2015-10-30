798c04e4f6c832aec5ffc589c1e14e572f6466ef
Aarni Koskela <aarni.koskela@andersinno.fi>
2015-10-30 09:28:21 +0200

    v0.0.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
 
 setup(
     name='hayes',
-    version='0.0.4.1',
+    version='0.0.5',
     packages=['hayes', 'hayes.analysis', 'hayes.ext', 'hayes.django_interop', 'hayes.search'],
     license='MIT',
     long_description="n/a",
