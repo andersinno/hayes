2755fc04d621ffdf7b750d6bf23f156e41f7e2db
Tuomas Suutari <tuomas.suutari@anders.fi>
2017-08-22 13:53:25 +0300

    setup: Move metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,20 +1,6 @@
-# coding=utf-8
-from setuptools import setup
+import setuptools
 
-requirements = [
-	line.decode("UTF-8")
-	for line
-	in open("requirements.txt", "rb")
-	if line.strip() and not line.startswith(b"#")
-]
-
-setup(
-    name='hayes',
-    version='0.0.5',
-    packages=['hayes', 'hayes.analysis', 'hayes.ext', 'hayes.django_interop', 'hayes.search'],
-    license='MIT',
-    long_description="n/a",
-    maintainer="Anders Innovations",
-    maintainer_email="support@anders.fi",
-    install_requires=requirements
-)
+if __name__ == '__main__':
+    setuptools.setup(
+        setup_requires=['setuptools>=34.0', 'setuptools-gitver'],
+        gitver=True)
